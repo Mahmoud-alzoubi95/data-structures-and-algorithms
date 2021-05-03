23415b3e141920cdd47d6d2dcacd3448797d7a92
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-05-03 23:32:58 +0300

    pass the test

--- python/challenges/array_shift/array_shift.py
+++ python/challenges/array_shift/array_shift.py
@@ -1,14 +1,19 @@
+
+
 def ShiftArray(arr,num):
+
   newarr = []
   i=0
 
   while i<len(arr):
 
     if arr[i]<num and arr[i+1]>num:
-      newList.append(arr[i])
-      newList.append(num)
+      newarr.append(arr[i])
+      newarr.append(num)
     else:
-      newList.append(arr[i])
+      newarr.append(arr[i])
     i+=1
   return newarr
+
+print(ShiftArray([2,4,6,8],5))
 
--- python/tests/test_reverse_linked_list.py
+++ python/tests/test_reverse_linked_list.py
@@ -1,5 +1,7 @@
 from code_challenges.reverse_linked_list import reverse_list
 
 
+
 def test_import():
     assert reverse_list
+
