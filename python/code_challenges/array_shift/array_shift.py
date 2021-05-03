cf6db6c34fea5f9db902b96581ff9ec7469ac976
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-05-03 21:46:36 +0300

    pass

--- python/code_challenges/array_shift/array_shift.py
+++ python/code_challenges/array_shift/array_shift.py
@@ -1,14 +1,14 @@
 def ShiftArray(arr,num):
-  newList = []
+  newarr = []
   i=0
 
   while i<len(arr):
-      
+
     if arr[i]<num and arr[i+1]>num:
       newList.append(arr[i])
       newList.append(num)
     else:
       newList.append(arr[i])
     i+=1
-  return newList
+  return newarr
 
